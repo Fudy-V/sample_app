e367405b8c970ac763f2328b71e108b4dff23986
Fudy-V <fudyV.test@gmail.com>
2024-11-01 01:43:10 +0900

    first commit

---
+++ lib/screens/search_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SearchScreen extends StatefulWidget {
+  const SearchScreen({super.key});
+
+  @override
+  State<SearchScreen> createState() => _SerchScreenState();
+}
+
+class _SerchScreenState extends State<SearchScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Qiita Seach"),
+      ),
+      body: Container(),
+    );
+  }
+}
